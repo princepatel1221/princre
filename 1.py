b4aa085f4b54c681c86922099d1711098e5c60e3
princepatel1221 <patelkano1221@gmail.com>
2025-05-30 09:54:22 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,7 @@
+a = input("Enter first number: ")
+b = input("Enter second number: ")
+
+# Print the entered numbers
+print("First number is:", a)
+print("Second number is:", b)
+
---
+++ 2.py
@@ -0,0 +1,13 @@
+num1= 5
+num2= 10
+
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2  # Make sure num2 is not zero
+
+
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
